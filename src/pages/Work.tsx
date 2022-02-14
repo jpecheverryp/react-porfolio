63628d94b64d41da24fa53ea2defe0b017d2efcf
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-02-14 18:11:54 -0500

    add work page

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,10 +1,81 @@
-import { Box, Container } from '@chakra-ui/react';
+import {
+  Box,
+  Container,
+  Grid,
+  GridItem,
+  Heading,
+  Image,
+  LinkBox,
+  Text,
+} from '@chakra-ui/react';
+import { Link as RouterLink } from 'react-router-dom';
+import digitalGardenThumb from '../images/digitalGardenThumb.png';
 import React from 'react';
 
 const Work = () => {
   return (
     <Container maxW={'container.sm'} pt={16}>
-      <Box>Work</Box>
+      <Heading as={'h2'} size='lg' textDecor={'underline'}>
+        Work
+      </Heading>
+      <Heading as={'h3'} size='md' mt={6} textDecor={'underline'}>
+        Featured
+      </Heading>
+      <LinkBox as={RouterLink} to='/work/digitalgarden'>
+        <Box mt={4}>
+          <Image
+            src={digitalGardenThumb}
+            borderRadius={'xl'}
+            alt={'Digital Garden Thumbnail'}
+          />
+          <Heading as={'h4'} size={'md'} mt={2} textAlign={'center'}>
+            Digital Garden
+          </Heading>
+          <Text textAlign={'center'}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            quibusdam aspernatur ad velit eum aut id ea asperiores, sed quidem
+            deleniti earum, repellendus nobis.
+          </Text>
+        </Box>
+      </LinkBox>
+      <Grid
+        mt={5}
+        templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
+        gap={2}
+      >
+        <LinkBox as={RouterLink} to='/work/digitalgarden'>
+          <GridItem w={'100%'} p={2}>
+            <Image
+              src={digitalGardenThumb}
+              borderRadius={'lg'}
+              alt={'Digital Garden Thumbnail'}
+            />
+            <Heading as={'h4'} size={'md'} mt={2} textAlign={'center'}>
+              Digital Garden
+            </Heading>
+            <Text textAlign={'center'}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+              quibusdam aspernatur ad velit eum
+            </Text>
+          </GridItem>
+        </LinkBox>
+        <LinkBox as={RouterLink} to='/work/digitalgarden'>
+          <GridItem w={'100%'} p={2}>
+            <Image
+              src={digitalGardenThumb}
+              borderRadius={'lg'}
+              alt={'Digital Garden Thumbnail'}
+            />
+            <Heading as={'h4'} size={'md'} mt={2} textAlign={'center'}>
+              Digital Garden
+            </Heading>
+            <Text textAlign={'center'}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+              quibusdam aspernatur ad velit eum
+            </Text>
+          </GridItem>
+        </LinkBox>
+      </Grid>
     </Container>
   );
 };
