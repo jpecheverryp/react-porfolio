e303542a25a397146aa4fd93cd7ae0d4529bd74b
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2022-02-14 21:26:22 -0500

    add credits on page footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Link, Text, useColorModeValue } from '@chakra-ui/react';
 
 const Footer = () => {
   return (
     <Box textAlign={'center'} mt={'8'}>
-      &copy; {new Date().getFullYear()} Juan P Echeverry. All Rights Reserved.
+      <Text>
+        &copy; {new Date().getFullYear()} Juan P Echeverry. All Rights Reserved.
+      </Text>
+      <Text>
+        Design inspired on{' '}
+        <Link
+          color={useColorModeValue('blue.600', 'orange.200')}
+          href='https://www.craftz.dog/'
+        >
+          Takuya Matsuyama's Homepage
+        </Link>
+      </Text>
     </Box>
   );
 };
